87f2a202ef4758c95e9a12ab510bd3beaa89fec6
amine019 <aminejendoubi8@gmail.com>
2022-01-12 23:55:25 +0100

    Add new files

---
+++ main.js
@@ -0,0 +1,13 @@
+var content = 'Hi, I am Amine Jendoubi a UX/UI & Graphic Designer based in Tunisia.';
+
+var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+
+$(ele).hide().appendTo('p').each(function (i) {
+    $(this).delay(100 * i).css({
+        display: 'inline',
+        opacity: 0
+    }).animate({
+        opacity: 1
+    }, 100);
+});
